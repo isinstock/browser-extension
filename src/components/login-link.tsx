ef426489fcb1e916811569d03e5158bbcabb5aee
Garrett Bjerkhoel <me@garrettbjerkhoel.com>
2023-06-20 21:47:49 -0700

    Update link

--- src/components/login-link.tsx
+++ src/components/login-link.tsx
@@ -1,7 +1,7 @@
 const LoginLink = () => {
   return (
     <a
-      href={`${ISINSTOCK_URL}/users/login?return_to=chrome-extension://jbokheigkkbbkonejaipdkhnbdefdegg/popup.html`}
+      href={`${ISINSTOCK_URL}/users/login?return_to=chrome-extension://bnglflgcpflggbpbcbpgeaknekceeojd/popup.html`}
       target="_blank"
       rel="noreferrer"
       class="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
