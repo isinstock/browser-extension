0d2b6ce96d440100e5f05e0ccae79e873c4eb3be
Garrett Bjerkhoel <me@garrettbjerkhoel.com>
2023-12-14 10:04:19 -0800

    Setup version

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -1,4 +1,3 @@
-declare const VERSION: string
 declare const ISINSTOCK_URL: string
 declare const CHROME_EXTENSION_ID: string
 
--- src/utils/fetch-api.ts
+++ src/utils/fetch-api.ts
@@ -1,19 +1,24 @@
 import browser from 'webextension-polyfill'
 
+import {getBrowserExtensionInstallToken} from './browser-extension-install-token'
+
+const browserExtensionInstallTokenHeader = 'X-Browser-Extension-Install-Token'
 const fetchApi = async (
   path: string,
-  method: 'POST' | 'GET',
+  method: 'POST' | 'GET' | 'PUT' | 'PATCH' | 'DELETE',
   body?: BodyInit | null | undefined,
   signal?: AbortSignal | null | undefined,
 ) => {
   const {accessToken} = await browser.storage.local.get({
     accessToken: '',
   })
+  const browserExtensionInstallToken = await getBrowserExtensionInstallToken()
 
   const headers: HeadersInit = {
     'Content-Type': 'application/json',
     Accept: 'application/json',
     'X-Extension-Version': VERSION,
+    [browserExtensionInstallTokenHeader]: browserExtensionInstallToken,
   }
   if (accessToken !== '' && accessToken !== null) {
     headers['Authorization'] = `Bearer ${accessToken}`
